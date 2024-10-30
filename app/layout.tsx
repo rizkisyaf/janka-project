a41d782614159e21feeee0ef6f58c2b835692134
rizkisyaf <rizkisyaf@gmail.com>
2024-10-30 17:21:31 +0700

    update meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,29 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Janka - Real-World Hedging Event Platform",
   description: "Janka enables users to hedge against real-world, uninsurable events with transparency, precision, and oracle-verified data.",
+  openGraph: {
+    title: "Janka - Real-World Hedging Event Platform",
+    description: "Janka enables users to hedge against real-world, uninsurable events with transparency, precision, and oracle-verified data.",
+    images: [
+      {
+        url: "/public/banner.png",
+        width: 1200,
+        height: 630,
+        alt: "Janka Banner",
+        type: "image/png",
+      },
+    ],
+    siteName: "Janka",
+    locale: "en_US",
+    type: "website",
+    url: "https://janka-project.vercel.app",
+  },
+  twitter: {
+    title: "Janka - Real-World Hedging Event Platform",
+    card: "summary_large_image",
+    description: "Janka enables users to hedge against real-world, uninsurable events with transparency, precision, and oracle-verified data.",
+    images: ["/public/banner.png"],
+  }
 };
 
 export default function RootLayout({
